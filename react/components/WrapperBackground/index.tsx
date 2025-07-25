e0e2ea4408a875c3f78b20a607375d369e875d5c
Leonard J. Ávila S <informatic.leonardavila@gmail.com>
2025-07-25 17:37:32 -0400

    update

--- react/components/WrapperBackground/index.tsx
+++ react/components/WrapperBackground/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {useCssHandles} from 'vtex.css-handles';
+import './index.css';
+
+const CSS_HANDLES = ['WrapperContainer', 'FullScreenImage'];
 
 function WrapperBackground() {
-  return <h1>Nutrisco Market</h1>;
+  const handles = useCssHandles(CSS_HANDLES);
+
+  return (
+    <div className={handles.WrapperContainer}>
+      <picture>
+        <source srcSet="https://orizoncl.vtexassets.com/arquivos/site-desktop-build-v2.jpg" media="(min-width: 1240px)" />
+        <img
+          src="https://orizoncl.vtexassets.com/arquivos/site-mobile-build-v2.jpg"
+          alt="Nutrisco Market"
+          className={handles.FullScreenImage}
+        />
+      </picture>
+    </div>
+  );
 }
 
 export default WrapperBackground;
